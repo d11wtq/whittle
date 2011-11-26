c574e88f94e6a82752c14965e940d2ab2c1c00e8
d11wtq <chris@w3style.co.uk>
2011-11-26 22:07:18 +1100

    Documentation

--- lib/whittle/rule_set.rb
+++ lib/whittle/rule_set.rb
@@ -1,22 +1,50 @@
+# Whittle: A little LALR(1) parser in pure ruby, without a generator.
+#
+# Copyright (c) Chris Corbyn, 2011
+
 module Whittle
+  # RuleSets are named collections of Rules.
+  #
+  # When you use the name of a rule in the grammar, you actually refer to the
+  # entire RuleSet and not an individual rule within it (unless of course, it
+  # only contains one Rule)
   class RuleSet
     include Enumerable
 
+    # Create a new RuleSet named +name+.
+    #
+    # @param [Symbol, String] name
+    #   the name of the rule in the grammar
     def initialize(name)
       @name  = name
       @rules = []
     end
 
+    # Enumerate all Rules in the set.
     def each(&block)
       @rules.each(&block)
     end
 
+    # Add a new Rule to the set.
+    #
+    # @param [Object...] components...
+    #   a variable list of components (Symbols, Strings, or Regexps)
     def [](*components)
       Rule.new(@name, *components).tap do |rule|
         @rules << rule
       end
     end
 
+    # Invoked during lexing, delegating to each rule in the set.
+    #
+    # @param [String] source
+    #   the complete input string
+    #
+    # @param [Fixnum] line
+    #   the current line number
+    #
+    # @return [Hash]
+    #   a Hash representing the found token, or nil
     def scan(source, line)
       each do |rule|
         if token = rule.scan(source, line)
@@ -27,6 +55,19 @@ module Whittle
       nil
     end
 
+    # Recursively builds the parse table into +table+.
+    #
+    # @param [Hash<Fixnum,Hash>] table
+    #   the parse table as constructed so far
+    #
+    # @param [Parser] parser
+    #   the parser containing the grammar
+    #
+    # @param [Hash] context
+    #   a Hash used to track state when building the parse table
+    #
+    # @return [Hash]
+    #   the parse table
     def build_parse_table(table, parser, context)
       return table if context[:seen].include?([context[:state], self])
 
@@ -39,10 +80,19 @@ module Whittle
       end
     end
 
+    # Predicate test for whether or not this RuleSet references a single
+    # terminal Symbol.
+    #
+    # @return [Boolean]
+    #   true if this rule is a terminal symbol
     def terminal?
       @rules.length == 1 && @rules.first.terminal?
     end
 
+    # Predicate test for whether or not this RuleSet references a nonterminal Symbol.
+    #
+    # @return [Boolean]
+    #   true if this rule is a nonterminal symbol
     def nonterminal?
       !terminal?
     end
